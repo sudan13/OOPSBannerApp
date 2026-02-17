eca399d1d320c4367989983e438f6a99c4f40e33
sud_an <sudan9280@gmail.com>
2026-02-17 22:14:36 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,4 @@
-/**
- * OOPSBannerApp UC1 – OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application
- * that displays the Object Oriented Programming System
- * OOPS acronym to the console.
- *
- * @author Developer
- * @version 1.0
- */
+
 
 public class OOPSBannerApp {
 
