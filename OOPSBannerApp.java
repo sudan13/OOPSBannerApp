289217f3eb8e8fcd2ced52540d77d8ad5fd13860
sudan13 <sudan9280@gmail.com>
2026-02-24 15:29:55 +0530

    UC6 - Refactored banner logic using static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,69 @@
+/**
+ * OOPSBannerApp UC6 – OOPS Banner Application (Use Case 6)
+ *
+ * This use case extends UC5 by implementing a modular approach
+ * to generate each letter's pattern through dedicated methods.
+ *
+ * @author Developer
+ * @version 6.0
+ */
+
 public class OOPSBannerApp {
 
+    // Method to generate the pattern for the letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "  *****  "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+                "*******  ",
+                "**    ** ",
+                "**    ** ",
+                "*******  ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+                "  ****** ",
+                " **    **",
+                "**       ",
+                "  *****  ",
+                "       **",
+                "**    ** ",
+                " ******  "
+        };
+    }
+
+    // Main method to run the banner display
     public static void main(String[] args) {
 
-        String[] lines = {
-            String.join(" ", " ***** ", " ***** ", " ***** ", " ***** "),
-            String.join(" ", " *   * ", " *   * ", " *     ", " *     "),
-            String.join(" ", " *   * ", " *   * ", " ***** ", " ***** "),
-            String.join(" ", " *   * ", " *   * ", " *     ", "     * "),
-            String.join(" ", " ***** ", " ***** ", " *     ", " ***** ")
-        };
+        // Declare String Arrays to hold patterns for each letter
+        String[] oPattern1 = getOPattern();
+        String[] oPattern2 = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        for(String line : lines) {
-            System.out.println(line);
+        // Loop to assemble and print each line of the banner
+        for (int i = 0; i < oPattern1.length; i++) {
+            System.out.println(oPattern1[i] + "  "
+                    + oPattern2[i] + "  "
+                    + pPattern[i] + "  "
+                    + sPattern[i]);
         }
     }
 }
