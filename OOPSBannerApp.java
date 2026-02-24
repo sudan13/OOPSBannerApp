d6864d96ad3ed2a53f16a34f73a36a0e91904726
sudan13 <sudan9280@gmail.com>
2026-02-24 14:57:00 +0530

    UC5: Inline array implementation using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,17 @@
-public static void main(String[] args) {
+public class OOPSBannerApp {
 
-    System.out.println("  *****     *****     ******      *****  ");
-    System.out.println(" *     *   *     *    *     *    *     * ");
-    System.out.println(" *     *   *     *    *     *    *       ");
-    System.out.println(" *     *   *     *    ******      *****  ");
-    System.out.println(" *     *   *     *    *                * ");
-    System.out.println(" *     *   *     *    *          *     * ");
-    System.out.println("  *****     *****     *           *****  ");
+    public static void main(String[] args) {
 
+        String[] lines = {
+            String.join(" ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join(" ", " *   * ", " *   * ", " *     ", " *     "),
+            String.join(" ", " *   * ", " *   * ", " ***** ", " ***** "),
+            String.join(" ", " *   * ", " *   * ", " *     ", "     * "),
+            String.join(" ", " ***** ", " ***** ", " *     ", " ***** ")
+        };
+
+        for(String line : lines) {
+            System.out.println(line);
+        }
+    }
 }
